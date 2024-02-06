c673e226992d921279d09666c9790a78b36dc189
Abhinay Kumar <129482893+abp012@users.noreply.github.com>
2024-02-06 22:24:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 
+        let textbox=document.getElementById("textbox");
+        textbox.addEventListener("input",function(){
+            let text=this.value;
+            //console.log(text);
+            let char=text.length;
+            //console.log(char);
+            document.getElementById("char").innerHTML=char;
+           // chars=char;
+           text=text.trim();
+           let words=text.split(" ");
+           //console.log(words);
+           let cleanlist=words.filter(function(elm){
+                return elm!="";
+           })
+           document.getElementById("words").innerHTML=cleanlist.length;
+        })
+   
